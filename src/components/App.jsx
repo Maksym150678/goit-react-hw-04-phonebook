badeeb690166fb361c56b24368850ca9c5c1a7a2
Maksym150678 <civicm110@gmail.com>
2022-09-02 16:53:34 +0300

    fixed app final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,55 @@
-import React from 'react';
-
-
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import { nanoid } from 'nanoid';
 import FormAddPhonebook from 'components/Phonebook/FormAddPhonebook/FormAddPhonebook';
 import PhonebookList from 'components/Phonebook/PhonebookList/PhonebookList';
 import styles from 'components/app.module.css';
 
-class App extends Component {
-  state = {
-    phonebook: [],
-    filter: '',
-  };
+const App = () => {
+  const [phonebook, setPhonebook] = useState(() => {
+    const value = JSON.parse(localStorage.getItem('phonebook'));
+    return value || [];
+  });
+  const [filter, setFilter] = useState('');
 
-  componentDidMount () {
-    const phonebook = JSON.parse(localStorage.getItem('phonebook'));
-    if(phonebook?.length) {
-      this.setState({
-      phonebook, 
-    })
-    }
-    
-  }
+  useEffect(() => {
+    localStorage.setItem('phonebook', JSON.stringify(phonebook));
+  }, [phonebook]);
 
-  componentDidUpdate (_, prevState) {
-    const {phonebook} = this.state;
-    if(prevState.phonebook !== phonebook){
-      localStorage.setItem('phonebook', JSON.stringify(phonebook));
-    }
-    
-  }
-
-  addContact = data => {
-    if (this.isDublicate(data)) {
+  const addContact = (data) => {
+    if(isDublicate(data)) {
       return alert(`${data.name} - ${data.number} is alredy in contacts.`);
     }
-    this.setState(prevState => {
+    setPhonebook(prevPhonebook => {
       const newContact = {
-        id: nanoid(),
         ...data,
+        id: nanoid(),
       };
-      return {
-        phonebook: [...prevState.phonebook, newContact],
-      };
-    });
+      return [...prevPhonebook, newContact];
+    })
   };
 
-  removeContact = id => {
-    this.setState(({ phonebook }) => {
-      const newPhonebook = phonebook.filter(item => item.id !== id);
-
-      return {
-        phonebook: newPhonebook,
-      };
-    });
-  };
-
-  handleFilter = ({ target }) => {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  isDublicate({ name, number }) {
-    const { phonebook } = this.state;
-    const result = phonebook.find(item => item.name === name && item.number === number);
-    return result;
+  const removeContact = (id) => {
+    setPhonebook(prevPhonebook => prevPhonebook.filter(item => item.id !== id));
   }
 
-  getFilteredPhonebook() {
-    const { phonebook, filter } = this.state;
+  const handleFilter = ({ target }) => {
+    setFilter(target.value);
+  }
 
+  const isDublicate = ({ name, number }) => {
+    const result = phonebook.find(
+      item => item.name === name && item.number === number
+    );
+    return Boolean(result);
+  };
+
+  const getFilteredPhonebook = () => {
     if (!filter) {
       return phonebook;
     }
 
-    const normalizedFilter = filter.toLocaleLowerCase();
+    const normalizedFilter = filter.toLowerCase();
 
     const filteredPhonebook = phonebook.filter(({ name, number }) => {
       const normalizedName = name.toLowerCase();
@@ -91,34 +63,30 @@ class App extends Component {
     return filteredPhonebook;
   }
 
-  render() {
-    const { addContact, removeContact, handleFilter } = this;
+  const filteredPhonebook = getFilteredPhonebook();
 
-    const phonebook = this.getFilteredPhonebook();
-
-    return (
-      <div className={styles.container}>
-        <h1 className={styles.title}>Phonebook</h1>
-        <div className={styles.row}>
-          <div className={styles.column}>
-            <FormAddPhonebook onSubmit={addContact} />
-          </div>
-          <div>
-            <PhonebookList items={phonebook} removeContact={removeContact} />
-            <input
-              name="filter"
-              onChange={handleFilter}
-              className={styles.filter}
-              placeholder="Filter"
-            />
-          </div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Phonebook</h1>
+      <div className={styles.row}>
+        <div className={styles.column}>
+          <FormAddPhonebook onSubmit={addContact} />
+        </div>
+        <div>
+          <PhonebookList
+            items={filteredPhonebook}
+            removeContact={removeContact}
+          />
+          <input
+            name="filter"
+            onChange={handleFilter}
+            className={styles.filter}
+            placeholder="Filter"
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
-
-
-
